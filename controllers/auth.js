5da902f5864de1bae5a977080cd63bf9d10f3226
User Name <annakirath@gmail.com>
2024-07-15 10:37:42 +0100

    error handling with try catch. Added to my sign up and sign in routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,35 +12,48 @@ router.get('/sign-in', (req, res) => {
 });
 
 router.post('/sign-up', async (req, res) => {
-    const userInDatabase = await User.findOne({ username: req.body.username });
-    if (userInDatabase) {
-        return res.send('Username already taken.');
-    }
-    if (req.body.password !== req.body.confirmPassword) { 
-        return res.send('Passwords do not match');
-    }
-    req.body.password = bcrypt.hashSync(req.body.password, 12);
-    const user = await User.create(req.body);
-    return res.send(`Thanks for signing up ${user.username}!`);
+    try {
+        const userInDatabase = await User.findOne({ username: req.body.username });
+        if (userInDatabase) {
+            return res.send('Username already taken.');
+        }
+        if (req.body.password !== req.body.confirmPassword) {
+            return res.send('Passwords do not match');
+        }
+        req.body.password = bcrypt.hashSync(req.body.password, 12);
+        const user = await User.create(req.body);
+        res.send(`Thanks for signing up ${user.username}!`);
 
+        //return res.redirect('/auth/sign-in') ---> figure out how to redirect to /auth/sign-in once user has received the thank you for signing up message above
+    } catch (error) {
+        console.log(error);
+        return res.redirect('/');
+    }
 });
 
 router.post("/sign-in", async (req, res) => {
-    const userInDatabase = await User.findOne({ username: req.body.username });
-    if (!userInDatabase) {
-        return res.send("Login failed. Please try again.");
-    }
-    const validPassword = bcrypt.compareSync(req.body.password, userInDatabase.password);
-    if (!validPassword) {
-        return res.send("Login failed. Please try again.");
-    }
+    try {
+        const userInDatabase = await User.findOne({ username: req.body.username });
+        if (!userInDatabase) {
+            return res.send("Login failed. Please try again.");
+        }
+        const validPassword = bcrypt.compareSync(req.body.password, userInDatabase.password);
+        if (!validPassword) {
+            return res.send("Login failed. Please try again.");
+        }
 
-    req.session.user = {
-        username: userInDatabase.username
+        req.session.user = {
+            username: userInDatabase.username
+        }
+        
+        req.session.save(() => {
+            res.redirect('/')
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
     }
-    req.session.save(() => { 
-        res.redirect('/')
-    });
 });
 
 router.get('/sign-out', (req, res) => {
