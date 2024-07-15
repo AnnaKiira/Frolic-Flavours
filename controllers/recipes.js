d4417c793270c45acd618ddfdd7019ae242900a6
User Name <annakirath@gmail.com>
2024-07-15 16:03:26 +0100

    using populate method to populate the owner, since owner is a required field in my schema. nut quite working yet. tryna debug and fix the issue

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const Recipe = require('../models/recipe.js')
+const Recipe = require('../models/recipe')
 
 //recipes/index
 router.get('/', async (req, res) => {
     try {
-        const allRecipes = await Recipe.find()
-        res.render('recipes/index', { recipes: allRecipes })
+        const allRecipes = await Recipe.find().populate('owner')
+        res.render('recipes/index', { recipes: allRecipes, })
     } catch (error) {
         console.log(error)
         res.redirect('/')
@@ -39,15 +39,15 @@ router.post('/', async (req, res) => {
 router.get('/:recipeId', async (req, res) => {
     try {
         const recipeId = req.params.recipeId
-        const foundRecipe = await Recipe.findById(recipeId)
-        
+        const foundRecipe = await Recipe.findById(recipeId).populate('owner')
+
         if (!foundRecipe) {
             const error = new Error('Recipe Not Found')
             error.status = 404
             throw error
         }
         res.render('recipes/show', {
-            foundRecipe
+            recipe: foundRecipe
         })
     } catch (error) {
         console.log(error)
