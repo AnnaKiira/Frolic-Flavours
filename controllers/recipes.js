e9aef280814f2416c1e649325cc07956c60f678b
User Name <annakirath@gmail.com>
2024-07-15 11:33:13 +0100

    set up the index, new and create routes. Filled in the code in my new.ejs file under views folder.

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -3,4 +3,38 @@ const router = express.Router()
 
 const Recipe = require('../models/recipe.js')
 
+//recipes/index
+router.get('/', async (req, res) => {
+    try {
+        const allRecipes = await Recipe.find()
+        res.render('recipes/index', { recipes: allRecipes })
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+})
+
+//recipes/new
+router.get('/new', (req, res) => {
+    try {
+        return res.render('recipes/new')
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+})
+
+//recipes/create
+router.post('/', async (req, res) => {
+    try {
+        const createRecipe = await Recipe.create(req.body)
+        res.redirect('/recipes')
+    } catch (error) {
+        console.log(error)
+        res.render('recipes/new', {errorMessage: error.message})
+    }
+})
+
+//recipes/
+
 module.exports = router
