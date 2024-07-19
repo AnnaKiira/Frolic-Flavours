a1d8da590721d1560ecbf45f577a0ac83075a86d
User Name <annakirath@gmail.com>
2024-07-19 13:19:34 +0100

    test

---
+++ netlify/functions/api.js
@@ -0,0 +1,67 @@
+const serverless = require('serverless-http')
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+const passUserToView = require('../../middleware/pass-user-to-view.js')
+const isSignedIn = require('../../middleware/is-signed-in.js')
+
+const authController = require('../../controllers/auth.js');
+const recipesController = require('../../controllers/recipes.js');
+const usersController = require('../../controllers/users.js');
+
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+app.use(express.static("public"));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ 
+        mongoUrl: process.env.MONGODB_URI,
+    }),
+  })
+);
+
+app.use(passUserToView);
+
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        user: req.session.user,
+    });
+});
+  
+app.use('/auth', authController);
+  
+app.use('/recipes', isSignedIn, recipesController);
+
+app.use('/users', isSignedIn, usersController);
+
+app.get('*', (req, res) => {
+    res.render('404')
+});
+
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('Database Connection Established')
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports.handler = serverless(app)
+
+connect()
